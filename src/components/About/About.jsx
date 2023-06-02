c84f175744c2af5ff64e9c60a376c54cf0d6b70f
Sebaroca00 <sebaroca00@gmail.com>
2023-06-02 01:51:31 -0300

    Avance Routing

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,24 @@
 import style from "./About.module.css";
-
+import Foto from '../Img/Foto About.jpg' 
 
 const About = () => {
     return(
         <div className={style.Contenedor}> 
         <hr />
-            <h2>Esto deberia ser un about me sobre Luciano Sebastian Roca</h2>
+            <h2 className={style.Title}>Bienvenidos a mi Proyecto Integrador</h2>
+            <div>
+                
+                    <div className={style.ContenedorDatos}>
+                        <h3 className={style.Datos}>Nombre: Sebastian Roca</h3>
+                        <h3 className={style.Datos}>Pais: Argentina</h3>
+                        <h3 className={style.Datos}>Edad: 22 Años</h3>
+                        <img className={style.Foto} src={Foto} alt="">
+                </img>
+                    </div>
+                
+            </div>
+            
+        
         <hr />
         </div>
     )
