fee71236fdec70351de1389bba6ef0ab5c0dfb53
Sebaroca00 <sebaroca00@gmail.com>
2023-06-30 18:24:39 -0300

    Express terminado

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -3,12 +3,13 @@ const users = require("../utils/user")
 const login = (req, res) => {
      const {email, password} = req.query
 
+let access = false
 users.forEach((user)=>{
     if(user.email === email && user.password === password) {
-        return res.status(200).json({ access : true });
+        access = true;
     }
 })
-return res.json({ access : false})
+return res.json({ access })
 }
 
 module.exports = login;
